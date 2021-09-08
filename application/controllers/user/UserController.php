8189d23f67821aec363244343c14bad4cb692214
Gian Akbar <akbarigi20@gmail.com>
2021-09-09 02:00:40 +0700

    3 - add update image
    tapi masih error

--- application/controllers/user/UserController.php
+++ application/controllers/user/UserController.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     }
     
     
-    public function index_post(){
+    public function userLogin_post(){
         $username = $this->input->post('username');
         $password = $this->input->post('password');
 
@@ -87,5 +87,58 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         }
 
     }
+
+    public function userDelete_post(){
+        $user_id = $this->input->post("user_id");
+        $result = $this->userApiModel->deleteUser($user_id);
+
+        if($result){
+            $this->response([
+                "status" => 1,
+                "message" => "Delete user success"
+            ], REST_Controller::HTTP_OK);
+        }else{
+            $this->response([
+                "status" => 0,
+                "message" => "Oops, delete failed"
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        }
+    }
+
+    public function userUpdateImage_post(){
+        $user_id = $this->input->post("user_id");
+        $config['upload_path'] = './image/users';
+        $config['allowed_types'] = 'jpg|png|jpeg';
+        $config['max_size'] = '1000'; // max size in KB
+        $config['overwrite'] = TRUE; // For Replace Image name
+        $config['encrypt_name'] = TRUE; // For Encrypt Name
+        $this->load->library('upload', $config);
+        $this->upload->initialize($config);
+        if (!$this->upload->do_upload('file')) {
+            $errorArray = strip_tags($this->upload->display_errors());
+            $this->response(
+                ["status" => 0, "message" => "Oops, failed! $errorArray"],
+                REST_Controller::HTTP_BAD_REQUEST
+            );
+        } else {
+            $info = $this->upload->data();
+            $image_path = $info['raw_name'] . $info['file_ext'];
+            $result = $this->userApiModel->userUpdateImage($user_id, $image_path);
+            if ($result) {
+                $updateUser = $this->userApiModel->getUserById($user_id);
+                $this->response([
+                    "status" => 1,
+                    "message" => "Succes update image",
+                    "data" => $updateUser
+                ], REST_Controller::HTTP_OK);
+            } else {
+                $this->response([
+                    "status" => 0,
+                    "message" => " Gagal Update Image Profil , Coba lagi nanti..."
+                ], REST_Controller::HTTP_BAD_REQUEST);
+            }
+        }
+    }
+
 }
 /** End of file UserController.php **/
--- application/models/UserApiModel.php
+++ application/models/UserApiModel.php
@@ -28,6 +28,63 @@ class UserApiModel extends CI_Model {
         return $query;
     }
 
+    public function userUpdateImage($user_id, $image)
+    {
+        $this->db->select('image');
+        $this->db->from('tbl_user');
+        $this->db->where('user_id', $user_id);
+        $selectOldImage = $this->db->get()->row()->image;
+        $selectNewImage = ['image' => $image];
+        $this->db->where('user_id', $user_id);
+        $query = $this->db->update('tbl_user', $selectNewImage);
+        if ($query) {
+            if (!empty($selectOldImage)) {
+                if (file_exists("./image/users/" . $selectOldImage)) {
+                    unlink("./image/users/" . $selectOldImage);
+                    return $query;
+                } else {
+                    return $query;
+                }
+            } else {
+                return $query;
+            }
+        } else {
+            return false;
+        }
+    }
+     public function getUserById($user_id)
+    {
+        $this->db->select('*');
+        $this->db->from('tbl_user');
+        $this->db->where('user_id', $user_id);
+        $query = $this->db->get()->result_array();
+        return $query;
+    }
+
+    public function deleteUser($user_id)
+    {
+        $this->db->select('image');
+        $this->db->from('tbl_user');
+        $this->db->where('user_id', $user_id);
+        $selectOldImage = $this->db->get()->row()->image;
+        $this->db->where('user_id', $user_id);
+        $query = $this->db->delete('tbl_user');
+        if ($query) {
+            if (!empty($selectOldImage)) {
+                if (file_exists("./image/users/" . $selectOldImage)) {
+                    unlink("./image/users/" . $selectOldImage);
+                    return $query;
+                } else {
+                    return $query;
+                }
+            } else {
+                return $query;
+            }
+        } else {
+            return false;
+        }
+    }
+
 }
 
 /* End of file UserApiModel.php */
